bdaaa2770e9b1d625ac33c98b58e5c9af3791f4b
TOSHIBA <ridwanfahmidina12@gmail.com>
2024-05-28 10:13:58 +0700

    menambahkan virtual method pesan

--- ParadigmaOOP2_D.cpp
+++ ParadigmaOOP2_D.cpp
@@ -3,6 +3,11 @@ using namespace std;
 
 class seseorang {
 public:
+	virtual void pesan() = 0;
+	// virtual void pesan()
+	// {
+	//      cout << "Pesan dari seseorang" << endl;
+	// }
 };
 
 int main() {
