325cf1bd2e5366ea65bcb78af0d6e978f87e1fc4
TOSHIBA <ridwanfahmidina12@gmail.com>
2024-05-28 10:37:51 +0700

    menyelesaikan project overriding

--- Overriding/Overriding.cpp
+++ Overriding/Overriding.cpp
@@ -8,11 +8,20 @@ using namespace std;
 class baseClass {
 public:
 	virtual void perkenalan() {
-		
+		cout << "Hallo saya Function dari base class";
 	}
+};
 
+class derivedClass : public baseClass {
+public:
+	void perkenalan() {
+		cout << "Hallo saya Function dari derived class";
+	}
 };
 
 int main() {
+	derivedClass a;
+	a.perkenalan();
 
+	return 0;
 }
